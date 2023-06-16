2016c8dfbab6309e8cb388cdc4249cf3b68b3600
Luyanda Sithole <luyandabrian@icloud.com>
2023-06-15 18:26:15 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 /**
- * main - A Program printing a line using printf function
- * Return 0 (Success)
+ * main - prints "with proper grammer, but the outcome is a piece of art,"
+ *Return: 0 (Success)
+ *
  */
+
 int main(void)
+
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return (0);
+	return (0);
 }
