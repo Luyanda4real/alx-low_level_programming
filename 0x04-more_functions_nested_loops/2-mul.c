883a246663c02be328ad2ce62c384a4435b29a33
Luyanda Sithole <luyandabrian@icloud.com>
2023-06-22 11:29:56 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two intergers
+ * @a: first interger
+ * @b: second interger
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
