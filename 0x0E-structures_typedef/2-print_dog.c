d0a94879c3c51b9c50ba2374996b0f8cee0c6797
Luyanda Sithole <luyandabrian@icloud.com>
2023-07-17 15:47:59 -0700

    2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -6,16 +6,15 @@
  * print_dog - Prints a struct dog
  * @d: The struct dog to be printed
  */
-
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
 
 	if (d->name == NULL)
-		printf("name: (nil)\n");
+		printf("Name: (nil)\n");
 	else
-		printf("name: %s\n", d->name);
+		printf("Name: %s\n", d->name);
 
 	if (d->age < 0)
 		printf("Age: (nil)\n");
